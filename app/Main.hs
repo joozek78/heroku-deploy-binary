33b4aa94d05307ba19c508e77462b09a75dea2df
Marcin Goliński <mj.golinski@gmail.com>
2015-08-19 23:33:45 +0200

    add support for explicit token passage

--- app/Main.hs
+++ app/Main.hs
@@ -36,6 +36,7 @@ instance FromJSON SlugInfo
 instance FromJSON BlobInfo
 
 type CurlOpt = String
+type HerokuToken = String
 
 forceDecode :: FromJSON a => LB.ByteString -> a
 forceDecode s = case eitherDecode s of
@@ -55,47 +56,54 @@ debugLn = hPutStrLn stderr
 main :: IO ()
 main = do
   args <- getArgs
-  case args of
-    [appName, execName] -> deploy appName execName None
-    ["-k", appName, execName] -> deploy appName execName Insecure
-    _ -> help
+  main' args None Nothing
+  where
+    main' :: [String] -> Flags -> Maybe HerokuToken -> IO ()
+    main' ("-k":args) _ t = main' args Insecure t
+    main' ("-t":token:args) f _ = main' args f $ Just token
+    main' [appName, execName] f t = deploy appName execName f t
+    main' _ _ _ = help
 
 help = do
   name <- getProgName
-  debugLn $ printf "usage: %s [-k] <heroku app name> <executable>" name
+  debugLn $ printf "usage: %s [-k] [-t <token>] <heroku app name> <executable>" name
   debugLn "-k: don't check ssl certificate when uploading slug (helps on windows)"
+  debugLn "-t <token>: use given heroku token"
 
-deploy appName execName flags = do
-  let baseCurlOpt = "-nsS" -- use netrc, be silent but show errors
-  let defCurlOpt = if flags == Insecure then baseCurlOpt ++ "k" else baseCurlOpt
+deploy :: String -> String -> Flags -> Maybe HerokuToken -> IO ()
+deploy appName execName flags maybeToken = do
+  let baseCurlOpt = ["-nsS"] -- use netrc, be silent but show errors
+  let defCurlOpt = if flags == Insecure then baseCurlOpt <> ["-k"] else baseCurlOpt
+  let herokuCurlOpt = case maybeToken of
+                        Nothing -> defCurlOpt
+                        Just token -> defCurlOpt <> ["-H", "Authorization: Bearer "++token]
   validateArgs
 
   debugLn $ printf "deploying executable '%s' to app %s" execName appName
-  !s@(SlugInfo (BlobInfo slugUrl) slugId) <- createSlug defCurlOpt execName appName
+  !s@(SlugInfo (BlobInfo slugUrl) slugId) <- createSlug herokuCurlOpt execName appName
   print $ "created slug: " ++ show s
   uploadSlug defCurlOpt slugUrl execName appName
-  releaseSlug defCurlOpt slugId appName
+  releaseSlug herokuCurlOpt slugId appName
   print "Done"
   where
     validateArgs = do
       execExists <- doesFileExist execName
       unless execExists $ error $ printf "File %s does not exist!" execName
 
-createSlug :: CurlOpt -> String -> String -> IO SlugInfo
-createSlug defCurlOpt execName appName = do
+createSlug :: [CurlOpt] -> String -> String -> IO SlugInfo
+createSlug curlOpt execName appName = do
   slugRespStr <- (let binary = "curl"
                       stdin = ""
-                      args = ["-X", "POST",
+                      args = curlOpt <> ["-X", "POST",
                        "-H", "Content-Type: application/json",
                        "-H", "Accept: application/vnd.heroku+json; version=3",
                        "-d", printf "{\"process_types\":{\"web\":\"./%s\"}}" $ takeFileName execName,
-                       defCurlOpt,
                        printf "https://api.heroku.com/apps/%s/slugs" appName]
                    in readProcess binary args stdin)
   return . forceDecode . LB.fromStrict . B.pack $ slugRespStr
 
-uploadSlug :: CurlOpt -> Text -> String -> String -> IO ()
-uploadSlug defCurlOpt slugUrl execName appName = do
+uploadSlug :: [CurlOpt] -> Text -> String -> String -> IO ()
+uploadSlug curlOpt slugUrl execName appName = do
   print $ "Packing binary " <> execName
   withSystemTempDirectory "heroku-deploy-binary" $ \workDir -> do
     createDirectory $ workDir </> "app"
@@ -103,23 +111,21 @@ uploadSlug defCurlOpt slugUrl execName appName = do
     let tgzFileName = workDir </> "slug.tgz"
     _ <- readCreateProcess (proc "tar" ["-cz", "./app", "-f", tgzFileName]) { cwd = Just workDir } ""
     print "Uploading binary"
-    callProcess "curl" ["-X", "PUT",
+    callProcess "curl" $ curlOpt <> ["-X", "PUT",
        "-H", "Content-Type:",
        "--data-binary", "@" ++ tgzFileName,
-       defCurlOpt,
        T.unpack slugUrl]
     print "Binary uploaded"
 
-releaseSlug :: CurlOpt -> Text -> String -> IO ()
-releaseSlug defCurlOpt slugId appName = do
+releaseSlug :: [CurlOpt] -> Text -> String -> IO ()
+releaseSlug curlOpt slugId appName = do
   print $ "Releasing slug " <> T.unpack slugId
   _ <- (let binary = "curl"
             stdin = ""
-            args = ["-X", "POST",
+            args = curlOpt <> ["-X", "POST",
               "-H", "Content-Type: application/json",
               "-H", "Accept: application/vnd.heroku+json; version=3",
               "-d", printf "{\"slug\":\"%s\"}" (T.unpack slugId),
-              defCurlOpt,
               printf "https://api.heroku.com/apps/%s/releases" appName]
          in readProcess binary args stdin)
   print "Slug released"
